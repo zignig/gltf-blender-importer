617583d6b9577ad50855c65f0ac7f2c2a210de34
David Mugnai <dvd@gnx.it>
2018-05-01 11:28:05 +0200

    configure the camera from the gltf data (#19)
    * configure the camera from the gltf data
    
    * camera/create_camera: intentionally return an uninitialized camera for an unexpected type
    
    * camera/create_camera: handle a missing zfar in the perspective data
    
    * camera/create_camera: fallback behavior to handle a missing apesctRatio
    
    * create_camera: the glTF yfov must clearly go to the property `angle_y`
    of the Blender camera, while the `aspectRatio`, if present, affects
    the resolution instead of the `angle_x` (since the latter is computed
    from the `angle_y` and the resolution in Blender)

--- addons/io_scene_gltf/camera.py
+++ addons/io_scene_gltf/camera.py
@@ -1,8 +1,38 @@
 import bpy
+import math
 
 
-def create_camera(op, idx):
-    camera = op.gltf['cameras'][idx]
-    name = camera.get('name', 'cameras[%d]' % idx)
-    data = bpy.data.cameras.new(name)
-    return data
+def create_camera(op, idx, scene):
+    """
+    create a new blender camera from the gltf data
+    """
+    data = op.gltf['cameras'][idx]
+    name = data.get('name', 'cameras[%d]' % idx)
+    camera = bpy.data.cameras.new(name)
+    if data["type"] == "orthographic":
+        camera.type = "ORTHO"
+        p = data["orthographic"]
+        camera.clip_start = p["znear"]
+        camera.clip_end = p["zfar"]
+        camera.ortho_scale = max(p["xmag"], p["ymag"])
+    elif data["type"] == "perspective":
+        camera.type = "PERSP"
+        p = data["perspective"]
+        camera.clip_start = p["znear"]
+        # according to the spec a missing zfar means "infinite"
+        camera.clip_end = p.get("zfar", math.inf)
+        camera.lens_unit = "FOV"
+        camera.angle_y = p["yfov"]
+
+        # The aspectRatio is optional and if given, it is used to change the
+        # width resolution
+        aspectRatio = p.get("aspectRatio")
+        if aspectRatio:
+            scene.render.resolution_x = scene.render.resolution_y * aspectRatio
+    else:
+        # this branch should never been taken since the only camera types are
+        # "orthographic" and "perspective"; but if the input document use an
+        # unexpected type we prefer to return an unitialized camera instead of
+        # stop the import with an error
+        pass
+    return camera
